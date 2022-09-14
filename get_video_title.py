9f58ed829792234c654a7d4828430cf43416b9f4
Allen Kennedy Jr <allen@kennedystuff.com>
2022-09-14 12:47:00 -0400

    Added lookups of youtube video titles.  Requires a config.py file with your youtube data v3 API key in it.

---
+++ get_video_title.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 11:31:06 2022
+
+@author: AllenKll
+"""
+
+""" 
+Must create a local 'config.py' with your google API key in it:
+api_key = "YOUR_API_KEY"
+"""    
+
+import requests
+import config
+
+def get_video_title(video_id):
+
+    try:
+        r = requests.get(f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q={video_id}&key={config.api_key}",timeout=2)
+    except:
+        print("Encountered an error with youtube.")
+        return "UNKNOWN - Youtube connection porblem"
+
+    if r.status_code != 200:
+        return "UNKNOWN - Youtube Error"
+    
+    items = r.json()["items"]
+    for item in items:
+        if item["id"]["kind"] != "youtube#video": continue
+        if item["id"]["videoId"]== video_id:
+            return(item["snippet"]["title"])
+    
+    return "UNKNOWN - Possibly Unlisted"
+
+if __name__ == "__main__":
+    print(get_video_title("dQw4w9WgXcQ"))
