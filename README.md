3e4a11c29f031c2b8a698f1e932d8c7239fca2c4
MajicGit <95971895+MajicGit@users.noreply.github.com>
2022-09-14 20:10:18 +0200

    Allows usage without Youtube API Key
    Updated readme a bit as well

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
  Uses the ticket lists to get a list of videos that have been played on JungleTV.
 
 Note that this list is in no way or form comprehensive. Errors can and will occur, additionally the ticket lists that this relies on don't date back to the initial creation of JungleTV. Thus be careful when relying on this for winning events or so. I take no guarantee for the correctness. 
+
+## To run yourself:
+
+1. (Optional) Create a file called "config.py". In this you need to specify your Youtube Data v3 API  as `api_key = "YOUR_API_KEY"`. Optionally you can also adjust the `MIN_PLAYS_FOR_LOOKUP` [value in this file](./jtv_lists.py), to change how many videos names are resolved. 
+2. Run `python3 jtv_lists.py` and wait.
--- jtv_lists.py
+++ jtv_lists.py
@@ -1,6 +1,11 @@
 import requests
-import get_video_title
 
+title_enabled = False 
+try:
+    import get_video_title
+    title_enabled = True 
+except:
+    print("Couldn't import get_video_title. Please add a config file with a google API key as described in get_video-title.py if you want this tool to add the names of the videos")
 MIN_PLAYS_FOR_LOOKUP = 30 # this is so you don't kill you API quota
 
 videos = {}
@@ -36,10 +41,12 @@ print("Got all the videos, saving to file")
 with open("Output.txt","w",encoding="utf-8") as file:
     file.write("Video ID, Number of times played, Title \n")
     for video_id,num_plays in sorted(videos.items(),key= lambda x:x[1],reverse=True): #Sort videos by number of plays
-        if num_plays > MIN_PLAYS_FOR_LOOKUP:
+        if num_plays > MIN_PLAYS_FOR_LOOKUP and title_enabled:
             title = get_video_title.get_video_title(video_id)
-        else:
+        elif title_enabled:
             title = "Too insignificant to look up."
+        else:
+            title = "No API key found"
         
         file.write(f'{video_id}, {num_plays}, "{title}" \n')
 
